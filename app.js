97867e08bb78328830e0ffc8c279c8e4ec9b85f4
Tanmay Aggarwal <tanmayagg.2005@gmail.com>
2025-01-05 23:57:54 +0530

    Added landing page

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ app.use((req, res, next) => {
 
   next();
 });
-
+app.get("/", (req, res) => {
+    res.render("home")})
 app.use("/listings", listingRouter);
 
 app.use("/listings/:id/reviews", reviewRouter);
